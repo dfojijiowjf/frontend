61b89f6ede4124e71a9c22d4821db97d9d9c12dc
dfojijiowjf <110518297+dfojijiowjf@users.noreply.github.com>
2022-10-20 21:20:45 +1100

    Logout Function

--- src/components/pages/header/Header.js
+++ src/components/pages/header/Header.js
@@ -3,12 +3,30 @@ import { Link } from 'react-router-dom'
 import './Header.css'
 
 function Header() {
+  const [loggedIn,setLoggedIn] = useState(false)
+  const {user,setUser} = useContext(UserContext)
+
+  useEffect(() => {
+    if(user != null) {
+      setLoggedIn(true)
+    } else {
+      setLoggedIn(false)
+    }
+  },[user])
+
+  const handleLogout = (e) => {
+    e.preventDefault()
+    localStorage.removeItem("access_token")
+    localStorage.removeItem("type")
+    setUser(null)
+  }
 
   return (
     <>
       <div className='header'>
         <div className='header inner'>
           <h1 id="logoText">UTS</h1>
+          {loggedIn == true ? <button className='logout' onClick={handleLogout}>Logout</button> : null}
         </div>
       </div>
     </>
